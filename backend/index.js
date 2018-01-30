617c3e68f126ca3365ba2eb92c0b58656de6e7e6
IntegraDev <integradev@iMac-de-Alexis.local>
2018-01-30 15:37:52 -0600

    un comentario

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-var app = require('./Rutas/Rutas.js');
-
-
-var port = process.env.PORT || 9000;
-app.listen(port, function() {
-    console.log('Api REST corriendo en http://localhost:' + port);
-});
