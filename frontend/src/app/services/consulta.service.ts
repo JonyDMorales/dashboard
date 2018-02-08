57465b246574a5cf16a330beff48b7c09286979f
IntegraPC09 <jonatan_moralest@hotmail.com>
2018-02-08 12:23:55 -0600

    Hola

--- frontend/src/app/services/consulta.service.ts
+++ frontend/src/app/services/consulta.service.ts
@@ -39,9 +39,8 @@ export class ConsultaService {
 
     public getGastoTotalTierra(alianza, persona, categoria) {
         const uri = this.url + '/tierra/gastototal';
-        return this._http.post(uri, { 'alianza': alianza, 'persona': persona, 'categoria': categoria } ).map((res) => {
-            return res;
-        });
+        return this._http.post(uri, { 'alianza': alianza, 'persona': persona, 'categoria': categoria } ).map( res => {
+            return res['total']; });
     }
 
     public getGastoCategoriaTierra(alianza, persona, categoria) {
