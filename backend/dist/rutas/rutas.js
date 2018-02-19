a1279f4ee6258be173c1c8930993aa29da474472
IntegraPC09 <jonatan_moralest@hotmail.com>
2018-02-19 17:30:36 -0600

    Hola

--- backend/dist/rutas/rutas.js
+++ backend/dist/rutas/rutas.js
@@ -1,6 +1,5 @@
 "use strict";
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const Eventos = require("../functions/eventos.functions");
 const Tierra = require("../functions/tierra.functions");
@@ -15,7 +14,14 @@ api.post('/tierra/consulta', Tierra.consulta);
 let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
 app.use('/', api);
 module.exports = app;
 //# sourceMappingURL=rutas.js.map
